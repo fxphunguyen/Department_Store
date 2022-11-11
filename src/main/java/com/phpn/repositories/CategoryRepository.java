8303e26c00f407a5f792b313bb07292194e37c3d
DoHongHuy <100512564+DoHongHuy@users.noreply.github.com>
2022-11-11 17:02:20 +0700

    huy_commit

--- src/main/java/com/phpn/repositories/CategoryRepository.java
+++ src/main/java/com/phpn/repositories/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.phpn.repositories;
 
+import com.phpn.dto.localtionRegion.LocationRegionResult;
 import com.phpn.repositories.model.Category;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Integer> {}
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    LocationRegionResult findById(Long id);
+}
--- src/main/java/com/phpn/services/locationRegion/LocationRegionServiceImpl.java
+++ src/main/java/com/phpn/services/locationRegion/LocationRegionServiceImpl.java
@@ -1,21 +1,44 @@
 package com.phpn.services.locationRegion;
 
 
+import com.phpn.dto.category.CategoryResult;
 import com.phpn.dto.localtionRegion.LocationRegionResult;
-import com.phpn.repositories.LocationRegionRepository;
+import com.phpn.mappers.CategoryMapper;
+import com.phpn.repositories.CategoryRepository;
+import com.phpn.repositories.model.LocationRegion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class LocationRegionServiceImpl implements LocationRegionService{
 
+@Autowired
+    CategoryRepository categoryRepository;
+
+
+@Autowired
+    CategoryMapper categoryMapper;
 
     @Override
-    public List<LocationRegionResult> findAllLocaRegion() {
+    public List<CategoryResult> findAllLocaRegion() {
+        return categoryRepository.findAll()
+                .stream()
+                .map(createLocationRegion -> categoryMapper.toDTO(createLocationRegion))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public LocationRegionResult createLocationRegion(LocationRegion locationRegion) {
         return null;
+    }
+
+    @Override
+    public LocationRegionResult findById(Long id) {
+        return categoryRepository.findById(id);
     }
 }
