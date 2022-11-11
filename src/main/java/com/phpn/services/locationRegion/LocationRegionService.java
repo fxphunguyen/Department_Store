e9d0d8660fe9fe9222145bd0423a9f7fa4bb1fce
Phuoc Nguyen <PhuocNguyen.081096@gmail.com>
2022-11-11 22:24:09 +0700

    Ngoc Tao fix Cu

--- src/main/java/com/phpn/services/locationRegion/LocationRegionService.java
+++ src/main/java/com/phpn/services/locationRegion/LocationRegionService.java
@@ -1,13 +1,18 @@
 package com.phpn.services.locationRegion;
 
-import com.phpn.dto.customer.CustomerCreate;
-import com.phpn.dto.customer.CustomerResult;
+import com.phpn.dto.category.CategoryResult;
 import com.phpn.dto.localtionRegion.LocationRegionResult;
 import com.phpn.repositories.model.LocationRegion;
 
 import java.util.List;
 
 public interface LocationRegionService {
-    List<LocationRegionResult> findAllLocaRegion();
+    List<CategoryResult> findAllLocaRegion();
+
+
+    LocationRegionResult createLocationRegion (LocationRegion locationRegion );
+
+    LocationRegionResult findById(Long id);
+
 
 }
