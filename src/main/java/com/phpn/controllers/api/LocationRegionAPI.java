da7e8ea1075d89c736da2387365372472c942b6a
DoHongHuy <100512564+DoHongHuy@users.noreply.github.com>
2022-11-15 09:09:59 +0700

    huy_commit

--- src/main/java/com/phpn/controllers/api/LocationRegionAPI.java
+++ src/main/java/com/phpn/controllers/api/LocationRegionAPI.java
@@ -1,10 +1,7 @@
 package com.phpn.controllers.api;
 
-<<<<<<< HEAD
-=======
 
 import com.phpn.dto.localtionRegion.LocationRegionResult;
->>>>>>> huy_dev
 import com.phpn.repositories.LocationRegionRepository;
 import com.phpn.repositories.model.LocationRegion;
 import com.phpn.services.locationRegion.LocationRegionService;
@@ -12,64 +9,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-=======
 import org.springframework.web.bind.annotation.PathVariable;
->>>>>>> huy_dev
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-<<<<<<< HEAD
-=======
 import java.util.Optional;
->>>>>>> huy_dev
 
 @RestController
 @RequestMapping("/api/locationRegion")
 public class LocationRegionAPI {
-<<<<<<< HEAD
 
 
 
     @Autowired
     LocationRegionService locationRegionService;
 
-=======
-    @Autowired
-     LocationRegionService locationRegionService;
->>>>>>> huy_dev
-
     @Autowired
     LocationRegionRepository locationRegionRepository;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> huy_dev
     @GetMapping()
     public ResponseEntity<?> findAllLocationReggion(){
         List<LocationRegion>locationRegions =locationRegionRepository.findAll();
    return  new ResponseEntity<>(locationRegions , HttpStatus.OK);
 
     }
-<<<<<<< HEAD
 
 
-
-//        LocationRegionResult locationRegion = locationRegionService.findById( id);
-
-//    @GetMapping("/{id}")
-//    public LocationRegionResult findLocationRegionByID(@PathVariable Integer id){
-//        LocationRegionResult locationRegion = locationRegionService.findById( id);
-//       return locationRegion;
-//    }
-=======
     @GetMapping("/{id}")
     public ResponseEntity<?> findLocationRegionByID(@PathVariable Integer id){
         LocationRegionResult locationRegionResult = locationRegionService.findById(id);
        return new ResponseEntity<>(locationRegionResult, HttpStatus.OK) ;
     }
->>>>>>> huy_dev
 
 }
