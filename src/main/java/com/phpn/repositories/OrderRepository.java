e9c600863da2c1f974c6bfbdd1edd61604c98e7c
Phuoc Nguyen <PhuocNguyen.081096@gmail.com>
2022-11-24 10:20:43 +0700

    no message

--- src/main/java/com/phpn/repositories/OrderRepository.java
+++ src/main/java/com/phpn/repositories/OrderRepository.java
@@ -1,20 +1,12 @@
 package com.phpn.repositories;
 
-<<<<<<< HEAD
 import com.phpn.repositories.model.Order;
 import com.phpn.repositories.model.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-=======
-import com.phpn.repositories.model.LocationRegion;
-import com.phpn.repositories.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository  extends JpaRepository<Order, Integer> {
->>>>>>> huy_dev
 }
