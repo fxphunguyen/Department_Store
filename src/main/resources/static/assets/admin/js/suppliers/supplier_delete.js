e211bddcd736965864d3485fddfc1ccd9bdb62e0
npquyngoc <np.quyngoc@gmail.com>
2022-11-24 15:46:36 +0700

    Updated deleteById()

---
+++ src/main/resources/static/assets/admin/js/suppliers/supplier_delete.js
@@ -0,0 +1,22 @@
+const deleteById = (supplierId) => {
+    $("#ward").empty();
+    return $.ajax({
+        "headers": {
+            "accept": "application/json",
+            "content-type": "application/json"
+        },
+        "type": "delete",
+        "url": "http://localhost:8080/api/admin/suppliers/" + supplierId + "/delete"
+    })
+    .done(() => {
+        window.location.href="/admin/suppliers";
+    })
+    .fail((jqXHR) => {
+        console.log("An error occurred, can not delete supplier with id: " + supplierId);
+    });
+}
+
+$("#btn-supplier-delete").on("click", () => {
+    console.log("Delete supplier successfully!");
+    deleteById($("#btn-supplier-delete").val());
+});
