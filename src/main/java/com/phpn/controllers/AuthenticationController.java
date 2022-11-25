85fe9f4a385e45d2ef7076df87c290083a57381a
npquyngoc <np.quyngoc@gmail.com>
2022-11-25 13:33:50 +0700

    Update authentication templates

---
+++ src/main/java/com/phpn/controllers/AuthenticationController.java
@@ -0,0 +1,22 @@
+package com.phpn.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/")
+public class AuthenticationController {
+
+    @GetMapping("/login")
+    public ModelAndView showLoginPage() {
+        return new ModelAndView("/admin/authentications/login");
+    }
+
+    @GetMapping("/register")
+    public ModelAndView showRegisterPage() {
+        return new ModelAndView("/admin/authentications/register");
+    }
+
+}
