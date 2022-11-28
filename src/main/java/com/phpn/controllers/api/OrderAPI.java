5d76c5a25aecfacb9652bfc998e41eeb62ad7de9
fxphunguyen <fxphunguyen@gmail.com>
2022-11-29 01:42:30 +0700

    phu update OrderAPI findAll

--- src/main/java/com/phpn/controllers/api/OrderAPI.java
+++ src/main/java/com/phpn/controllers/api/OrderAPI.java
@@ -1,16 +1,16 @@
 package com.phpn.controllers.api;
 
 import com.phpn.dto.order.OrderParam;
+import com.phpn.dto.order.OrderResult;
 import com.phpn.services.customer.CustomerService;
 import com.phpn.services.employee.EmployeeService;
 import com.phpn.services.order.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -25,8 +25,16 @@ public class OrderAPI {
     @Autowired
     private CustomerService customerService;
 
+    @GetMapping("")
+    public ResponseEntity<?> findAll() {
+        List<OrderResult> orderResults = orderService.findAll();
+
+        return new ResponseEntity<>(orderResults, HttpStatus.OK);
+    }
+
     @PostMapping("/create")
     public ResponseEntity<?> createOrder(@RequestBody OrderParam orderParam) {
+
         return new ResponseEntity<>(orderService.createOrderExport(orderParam), HttpStatus.OK);
     }
 
