3b91ee48e3126f564acd03964c137f7917e1f7c2
fxphunguyen <fxphunguyen@gmail.com>
2022-11-28 17:41:09 +0700

    phu commit

--- src/main/java/com/phpn/dto/order/OrderParam.java
+++ src/main/java/com/phpn/dto/order/OrderParam.java
@@ -1,11 +1,6 @@
 package com.phpn.dto.order;
 
-import com.phpn.dto.customer.CustomerParam;
-import com.phpn.dto.employee.EmployeeParam;
 import com.phpn.dto.orderItem.OrderItemExport;
-import com.phpn.dto.orderItem.OrderItemParam;
-import com.phpn.dto.payment_methods.PaymentMethodParam;
-import com.phpn.repositories.model.Item;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
