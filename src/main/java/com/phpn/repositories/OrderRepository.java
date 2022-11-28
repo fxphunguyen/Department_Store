be6b98d1cf0aba788127a38f17aca9d79453af3d
fxphunguyen <fxphunguyen@gmail.com>
2022-11-28 11:04:00 +0700

    phu commit paymentOrderDTO

--- src/main/java/com/phpn/repositories/OrderRepository.java
+++ src/main/java/com/phpn/repositories/OrderRepository.java
@@ -1,7 +1,6 @@
 package com.phpn.repositories;
 
 import com.phpn.repositories.model.Order;
-import com.phpn.repositories.model.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
