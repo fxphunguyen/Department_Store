213153f662cd529b0f87c149c66f1cd882b5ee27
fxphunguyen <fxphunguyen@gmail.com>
2022-11-28 11:16:34 +0700

    Phú update OrderSevice

---
+++ src/main/java/com/phpn/services/order/OrderService.java
@@ -0,0 +1,13 @@
+package com.phpn.services.order;
+
+import com.phpn.dto.order.OrderParam;
+import com.phpn.dto.order.OrderResult;
+
+import java.util.List;
+
+public interface OrderService {
+
+    List<OrderResult> findAll();
+    
+    OrderResult createOrderExport(OrderParam orderParam);
+}
