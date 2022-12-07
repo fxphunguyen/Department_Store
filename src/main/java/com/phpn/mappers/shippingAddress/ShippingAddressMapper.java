e0d8c9b4c478222e64af0089f13f6ed9510d86a2
Phuoc Nguyen <PhuocNguyen.081096@gmail.com>
2022-12-07 14:50:04 +0700

    cmiit js

--- src/main/java/com/phpn/mappers/shippingAddress/ShippingAddressMapper.java
+++ src/main/java/com/phpn/mappers/shippingAddress/ShippingAddressMapper.java
@@ -23,7 +23,8 @@ public class ShippingAddressMapper {
                 .setProvinceName(shippingAddress.getProvinceName())
                 .setZipCode(shippingAddress.getZipCode())
                 .setEmail(shippingAddress.getEmail())
-                .setMobile(shippingAddress.getMobile());
+                .setMobile(shippingAddress.getMobile())
+                .setIsDefault(shippingAddress.getIsDefault());
 
     }
 
