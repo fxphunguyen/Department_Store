570bd66a48be6997c3aef7a872b246f0b2208486
dinhquocnhat <nhatd87@gmail.com>
2022-12-14 13:35:18 +0700

    nhat chinh sua create va lam check box list product

--- src/main/resources/static/assets/admin/js/product/app.js
+++ src/main/resources/static/assets/admin/js/product/app.js
@@ -21,7 +21,7 @@ class App {
     static IziToast = class {
         static showSuccessAlert(m) {
             iziToast.success({
-                title: 'OK',
+                title: 'Thành công: ',
                 position: 'topRight',
                 timeout: 2500,
                 message: m
@@ -30,7 +30,7 @@ class App {
 
         static showErrorAlert(m) {
             iziToast.error({
-                title: 'Error',
+                title: 'Lỗi: ',
                 position: 'topRight',
                 timeout: 2500,
                 message: m
@@ -41,7 +41,7 @@ class App {
     static renderRowProduct(item, showStatus) {
         let str = `
              <tr id="tr_${item.id}" >
-                <td class="align-middle"><input type="checkbox" value="${item.id}"></td>
+                <td class="align-middle"><input class="selectCheckbox"  name="options[]" type="checkbox" value="${item.id}"></td>
                 <td class="align-middle"><img width="50px" height="40px" src=${item.image} alt="image"></td>
                 <td class="align-middle"><a href="/admin/product/${item.id}" style="text-decoration: none">${item.title}</a></td>
                 <td class="align-middle">${item.category.name}</td>
